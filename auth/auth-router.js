f041dfc9e5dc28a9e44788f0f05aed3200cc0560
Latosha Tims <ltims080913@hotmail.com>
2020-05-22 20:26:10 -0700

    set up file structure. server and index files complete. api running. register and login endpoints functional.

---
+++ auth/auth-router.js
@@ -0,0 +1,61 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const Users = require('../users/user-model')
+const {isValid} = require('../users/user-services')
+
+router.post('/register', (req, res) => {
+    const credentials = req.body
+
+    if(isValid(credentials)){
+        const rounds = process.env.BCRYPT_ROUNDS || 8
+        const hash = bcrypt.hashSync(credentials.password, rounds)
+        credentials.password = hash
+        Users.add(credentials)
+        .then(user => {
+            res.status(201).json({data: user})
+        })
+        .catch(err => {
+            res.status(500).json({error: err.message})
+        })
+    } else {
+        res.status(400).json({message: 'Please provide username and password'})
+    }
+})
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body
+
+    if(isValid(req.body)){
+        Users.findBy({ username: username })
+        .then(([user]) => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                const token = createToken(user)
+                res.status(200).json({ message: 'User successfully logged In', token })
+            } else {
+                res.status(401).json({ message: 'Invalid credentials' })
+            }
+        })
+        .catch(er => {
+            res.status(500).json({ error: er.message })
+        })
+    } else {
+        res.status(400).json({ message: 'Please provide username and password'})
+    }
+})
+
+function createToken(user){
+    const payload = {
+        sub: user.id,
+        username: user.username,
+        password: user.password
+    }
+    const secret = process.env.JWT_SECRET || '2canKeepAsecretIf1ofThemIsDead'
+    const options = {
+        expiresIn: '1d'
+    }
+    return jwt.sign(payload, secret, options)
+}
+
+module.exports = router;
---
+++ users/user-router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+
+const Users = require('./user-model')
+const restricted = require('../auth/auth-middleware')
+
+router.use(restricted)
+
+router.get('/', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
+module.exports = router
